7920f0085286476175eac7f06867b7c22188afed
Muhammad Safyan Mehar <msafyan46@gmail.com>
2023-06-12 23:10:39 +0100

    server url changed

--- src/config/apollo.js
+++ src/config/apollo.js
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: "http://localhost:5000/graphql", // Replace with your GraphQL server URL
+  uri: "http://134.209.22.223/graphql", // Replace with your GraphQL server URL
   cache: new InMemoryCache(),
 });
