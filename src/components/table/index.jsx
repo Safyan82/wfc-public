0fff1c29d57daed086b637d7179430d1e7885503
Muhammad Safyan Mehar <msafyan46@gmail.com>
2023-06-27 16:19:20 +0100

    change checkbox intermediate design

--- src/components/table/index.jsx
+++ src/components/table/index.jsx
@@ -15,83 +15,41 @@ const DataTable = ({header, data}) => {
       title: 'Branch Name',
       dataIndex: 'branchName',
       key: 'branchName',
-      // sorter: (a, b) => a.branchName.length - b.branchName.length,
-      // sortOrder: sortedInfo.columnKey === 'branchName' ? sortedInfo.order : null,
-      // ellipsis: true,
     },
     {
       title: 'Post code',
       dataIndex: 'postCode',
       key: 'postCode',
-      // sorter: (a, b) => a.postCode - b.postCode,
-      // sortOrder: sortedInfo.columnKey === 'postCode' ? sortedInfo.order : null,
-      // ellipsis: true,
     },
     {
       title: 'Address Line 1',
       dataIndex: 'AddressLine1',
-      key: 'AddressLine1',
-      // sorter: (a, b) => a.postCode - b.postCode,
-      // sortOrder: sortedInfo.columnKey === 'postCode' ? sortedInfo.order : null,
-      // ellipsis: true,
+      key: 'AddressLine1'
     },
     {
       title: 'Address Line 2',
       dataIndex: 'AddressLine2',
       key: 'AddressLine2',
-      // sorter: (a, b) => a.postCode - b.postCode,
-      // sortOrder: sortedInfo.columnKey === 'postCode' ? sortedInfo.order : null,
-      // ellipsis: true,
     },
     {
       title: 'City',
       dataIndex: 'City',
       key: 'City',
-      // sorter: (a, b) => a.postCode - b.postCode,
-      // sortOrder: sortedInfo.columnKey === 'postCode' ? sortedInfo.order : null,
-      // ellipsis: true,
     },
   ]);
 
   const [dataSource, setDataSource] = useState([]);
 
-  const formateColumn = (str) => {
-    return str.replace(/([A-Z])/g, ' $1').trim();
-  }
   
   useEffect(()=>{
-    setDataSource(data?.branches.map(key => {
+    setDataSource(data?.branches.map((key,index) => {
       const {metadata, ...rest} = key;
-      return {...metadata, ...rest};
+      return {key:index ,...metadata, ...rest};
     }));
   },[data?.branches])
 
 
   useState(()=>{console.log(dataSource)},[dataSource])
-
-  // useEffect(()=>{
-  //   setLoading(true);
-  //   // console.log(data?.branches[0]?.metadata);
-  //   // let columns = [];
-  //   // // Object.keys(data?.branches[0]?.metadata)?.forEach((meta)=>{
-  //   // //   columns.push({
-  //   // //     title: formateColumn(meta),
-  //   // //     dataIndex: meta,
-  //   // //     key: meta,
-  //   // //   });
-  //   // // });
-  //   // // setDynamicColumn([
-  //   // //   ...dynamicColumn,
-  //   // //   ...columns,
-  //   // // ]);
-  //   setLoading(false);
-    
-  // },[data?.branches]);
-
-  // useEffect(()=>{
-  //   console.log(dynamicColumn);
-  // },[dynamicColumn]);
-
 
 
   const {
@@ -102,20 +60,14 @@ const DataTable = ({header, data}) => {
 
 
   const onSelectChange = (newSelectedRowKeys) => {
-        console.log('selectedRowKeys changed: ', newSelectedRowKeys);
         setSelectedRowKeys(newSelectedRowKeys);
   };
 
 
   // normal row selection
   const rowSelection = {
-    onChange: (selectedRowKeys, selectedRows) => {
-      console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
-    },
-    getCheckboxProps: (record) => ({
-      disabled: record.name === 'Disabled User', // Column configuration not to be checked
-      name: record.name,
-    }),
+    selectedRowKeys,
+    onChange: onSelectChange,
   };
 
 
@@ -127,15 +79,9 @@ const DataTable = ({header, data}) => {
             {!loading &&
             <Table  
               bordered
-              rowSelection={
-                {
-                  type: 'Checkbox',
-                  ...rowSelection,
-                }
-              } 
+              rowSelection={rowSelection}
               columns={dynamicColumn} 
               dataSource={dataSource} 
-              onChange={handleChange}
               pagination={{pageSize:5}}
               title={!header? null : () => {
                 if(header){
---
+++ src/pages/setting/setting.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Setting=()=>{
+    return(
+        <Row>
+            <Col span={3}>asd</Col>
+            <Col span={9}>cha</Col>
+        </Row>
+    );
+};
