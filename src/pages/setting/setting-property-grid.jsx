91d5026572c11657b108e1a3aa82384de229b427
Muhammad Safyan Mehar <msafyan46@gmail.com>
2023-07-03 02:02:02 +0100

    grid option added on hover

--- src/pages/setting/setting-property-grid.jsx
+++ src/pages/setting/setting-property-grid.jsx
@@ -1,4 +1,6 @@
-import { Button, Space, Table } from 'antd';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Button, Popover, Space, Table } from 'antd';
 import { useState } from 'react';
 const data = [
   {
@@ -12,6 +14,7 @@ const data = [
 export const SettingPropertyGrid = () => {
   const [filteredInfo, setFilteredInfo] = useState({});
   const [sortedInfo, setSortedInfo] = useState({});
+  const [hoveredRow, setHoveredRow] = useState(null);
   const handleChange = (pagination, filters, sorter) => {
     console.log('Various parameters', pagination, filters, sorter);
     setFilteredInfo(filters);
@@ -25,6 +28,58 @@ export const SettingPropertyGrid = () => {
       key: 'name',
       sorter: (a, b) => a.name.length - b.name.length,
       sortOrder: sortedInfo.columnKey === 'name' ? sortedInfo.order : null,
+      width:370,
+      render: (_, record) => {
+        console.log(record, "record")
+        const showActions = hoveredRow === record.key;
+        return (
+          <div style={{display:'flex', alignItems:'center', columnGap:'7px'}}>
+              <div>
+                <span className='record-title'>{record.name}</span>
+                <div className='record-subtitle'>single line text</div>
+              </div> &nbsp;
+               {showActions && <button className="grid-sm-btn" type="link" onClick={() => handleEdit(record)}>
+                  Edit
+                </button>}
+                {showActions && <button  className="grid-sm-btn" type="link" onClick={() => handleDelete(record)}>
+                  Clone
+                </button>}
+                {showActions && 
+                <Popover
+                  overlayClassName='settingCustomPopover'
+                  trigger={"click"}
+                  content={
+                    <div className="popover-data">
+                      <div className="popoverdataitem" >
+                          Edit
+                      </div>
+                      <div className="popoverdataitem" >
+                          Clone
+                      </div>
+                      <div className="popoverdataitem" >
+                          Move to group
+                      </div>
+                      <div className="popoverdataitem" >
+                          Export property history
+                      </div>
+                      <div className="popoverdataitem" >
+                          Assign Users & Teams &nbsp;<FontAwesomeIcon icon={faLock}/>
+                      </div>
+                      <div className="popoverdataitem" >
+                          Archive
+                      </div>
+                    </div>
+                  }
+                >
+
+                  <button className="grid-sm-btn" type="link" onClick={() => handleDelete(record)}>
+                    <div style={{display: 'flex', gap: '0'}}>More <span className='caret'></span></div>
+                  </button>
+                </Popover>
+                }
+          </div>
+        );
+      },
 
     },
     {
@@ -65,6 +120,25 @@ export const SettingPropertyGrid = () => {
   };
   
   
+  const handleEdit = (record) => {
+    // handle edit action for the selected record
+  };
+
+  const handleDelete = (record) => {
+    // handle delete action for the selected record
+  };
+
+  const rowClassName = (record) => {
+    return record.key === hoveredRow ? 'hovered-row' : '';
+  };
+  const handleRowMouseEnter = (record) => {
+    setHoveredRow(record.key);
+  };
+
+  const handleRowMouseLeave = () => {
+    setHoveredRow(null);
+  };
+  
   return (
     <div 
     className='setting-grid'>
@@ -72,7 +146,15 @@ export const SettingPropertyGrid = () => {
         columns={columns} 
         dataSource={data} 
         rowSelection={rowSelection}
-        onChange={handleChange} />
+        onChange={handleChange} 
+        
+        onRow={(record) => ({
+          onMouseEnter: () => handleRowMouseEnter(record),
+          onMouseLeave: () => handleRowMouseLeave(),
+        })}
+        rowClassName={rowClassName}
+
+        />
     </div>
   );
 };
