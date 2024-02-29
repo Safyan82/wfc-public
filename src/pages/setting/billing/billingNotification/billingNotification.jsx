2a8d85fdd985057d6f893b7330018878d23c7974
Muhammad Safyan Mehar <msafyan46@gmail.com>
2024-02-29 16:53:19 +0000

    skill grid updated

---
+++ src/pages/setting/billing/billingNotification/billingNotification.jsx
@@ -0,0 +1,31 @@
+export const BillingNotification = ()=>{
+    return(
+        <div className='setting-body'>
+            <div className='setting-body-inner'>
+                <div className="setting-body-inner">
+                    <div className="setting-body-title">
+                        <div className='setting-body-inner-title'>
+                            Billing Notifications
+                        </div>
+                    </div>
+                    
+                    <div className="text">
+                        Empower your subscription experience with our add-ons billing notifications, providing real-time updates to keep you in the loop and in control of your service charges.
+                    </div>
+
+
+                    {/* body */}
+
+                    
+
+
+                </div> 
+
+                                
+            </div>
+        </div>
+                
+    )
+        
+    
+}
---
+++ src/pages/setting/billing/paymentMethod/paymentMethods.jsx
@@ -0,0 +1,29 @@
+export const PaymentMethod = ()=>{
+    return(
+        <div className='setting-body'>
+            <div className='setting-body-inner'>
+                <div className="setting-body-inner">
+                    <div className="setting-body-title">
+                        <div className='setting-body-inner-title'>
+                            Payment Methods
+                        </div>
+                    </div>
+
+                    <div className="text">
+                        Diversify your payment choices for enhanced flexibility, making it effortless to integrate and enjoy our add-on services with a range of secure and convenient payment methods.
+                    </div>
+
+
+                    {/* body */}
+
+                    
+
+
+                </div> 
+
+                             
+            </div>
+        </div>
+        
+    )
+}
